565d5d2c3285808db01d6d5ca385bebbdb1309c2
mauricio lopez <mlopez@dixian.info>
2016-06-09 16:08:55 -0500

    New strategy to display Instagram photos from Drupal.

--- sites/all/themes/omega-efk/templates/views-view--instagram-feeds--block.tpl.php
+++ sites/all/themes/omega-efk/templates/views-view--instagram-feeds--block.tpl.php
@@ -27,163 +27,104 @@
  * @ingroup views_templates
  */
 ?>
-<?php 
-	$content = $view->style_plugin->rendered_fields;
-	//dpm($content);
+<?php
+$content = $view->style_plugin->rendered_fields;
+//var_dump(json_encode($content));die;
 ?>
 
 
 <link rel="stylesheet" type="text/css" href="/css/demo_grid.css" />
-		<link rel="stylesheet" type="text/css" href="/css/style_grid.css" />
-		<script type="text/javascript" src="/js/modernizr.custom.26633.js"></script>
-		
+<link rel="stylesheet" type="text/css" href="/css/style_grid.css" />
+<script type="text/javascript" src="/js/modernizr.custom.26633.js"></script>
+
 <div class="<?php print $classes; ?>">
-  <?php print render($title_prefix); ?>
-  <?php if ($title): ?>
-    <?php print $title; ?>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-   
+    <?php print render($title_prefix); ?>
+    <?php if ($title): ?>
+        <?php print $title; ?>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
+
     <?php if ($exposed): ?>
     <div class="view-filters clearfix">
-      <?php print $exposed; ?>
-  <?php endif; ?>
+        <?php print $exposed; ?>
+        <?php endif; ?>
 
-  <?php if ($header): ?>
-    <div class="view-header clearfix">
-      <?php print $header; ?>
-  <?php endif; ?>
+        <?php if ($header): ?>
+        <div class="view-header clearfix">
+            <?php print $header; ?>
+            <?php endif; ?>
 
 
-  <?php if ($attachment_before): ?>
-    <div class="attachment attachment-before">
-      <?php print $attachment_before; ?>
-  <?php endif; ?>
+            <?php if ($attachment_before): ?>
+            <div>
+                <?php print $attachment_before; ?>
+                <?php endif; ?>
 
-  <?php if ($rows): ?>
-		
-  <div style="width:100%;">
-     <div id="ri-grid" class="item-list ri-grid ri-grid-size-3 ri-shadow"> <img src="/images/loading.gif" class="ri-loading-image"> 
-	 <ul>  
-	  <?php //print $rows; 
-	  		foreach($content as $item){
-				echo '<li>'.$item['nothing'].'</li>';
-			}
-	  ?>
-	  </ul>
-	 </div>
-</div>    
-      <?php //print $rows; ?>
-  <?php elseif ($empty): ?>
-    <div class="view-empty">
-      <?php print $empty; ?>
-    </div>
-  <?php endif; ?>
+                <?php if ($rows): ?>
 
-  <?php if ($pager): ?>
-  <div class="clearfix"></div>
-    <?php print $pager; ?>
-	<div class="clearfix"></div>
-  <?php endif; ?>
+                    <div style="width:100%;">
+                        <div id="ri-grid" class="item-list ri-grid-size-3 ri-shadow"> <img src="/images/loading.gif" class="ri-loading-image">
+                            <ul>
+                                <?php //print $rows;
+                                foreach($content as $item){
+                                    echo '<li>'.$item['nothing'].'</li>';
+                                }
+                                ?>
+                            </ul>
+                        </div>
+                    </div>
+                    <?php //print $rows; ?>
+                <?php elseif ($empty): ?>
+                    <div class="view-empty">
+                        <?php //print $empty; ?>
+                    </div>
+                <?php endif; ?>
 
-  <?php if ($attachment_after): ?>
-    <div class="attachment attachment-after">
-      <?php print $attachment_after; ?>
-    </div>
-  <?php endif; ?>
+                <?php if ($pager): ?>
+                    <div class="clearfix"></div>
+                    <?php print $pager; ?>
+                    <div class="clearfix"></div>
+                <?php endif; ?>
 
-  <?php if ($more): ?>
-    <?php print $more; ?>
-  <?php endif; ?>
+                <?php if ($attachment_after): ?>
+                    <div class="attachment attachment-after">
+                        <?php print $attachment_after; ?>
+                    </div>
+                <?php endif; ?>
 
-  <?php if ($footer): ?>
-    <div class="view-footer">
-      <?php print $footer; ?>
-    </div>
-  <?php endif; ?>
+                <?php if ($more): ?>
+                    <?php print $more; ?>
+                <?php endif; ?>
 
-  <?php if ($feed_icon): ?>
-    <div class="feed-icon">
-      <?php print $feed_icon; ?>
-    </div>
-  <?php endif; ?>
+                <?php if ($footer): ?>
+                    <div class="view-footer">
+                        <?php print $footer; ?>
+                    </div>
+                <?php endif; ?>
 
-</div><?php /* class view */ ?>
-	
- <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-		<script type="text/javascript" src="/js/jquery.gridrotator.js"></script>
-		<script type="text/javascript">	
-			$(function() {
-			
-				$( '#ri-grid' ).gridrotator( {
-					// number of rows
-rows			: 2,
+                <?php if ($feed_icon): ?>
+                    <div class="feed-icon">
+                        <?php print $feed_icon; ?>
+                    </div>
+                <?php endif; ?>
 
-// number of columns 
-columns			: 6,
+            </div><?php /* class view */ ?>
 
-// rows/columns for different screen widths
-// i.e. w768 is for screens smaller than 768 pixels
-w1024			: {
-	rows	: 4,
-	columns	: 3
-},
 
-w768			: {
-	rows	: 3,
-	columns	: 4
-},
+            <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+            <script type="text/javascript">
+                window.onload = function() {
+                    $count=1;
+                    $('#ri-grid ul>li>a>img').each(function(){
+                        if($(this).css("width") == '20px' || $count>12){
+                            $(this).closest('li').css('display','none');
+                        }else{
+                            $count++;
+                        }
+                    });
 
-w480			: {
-	rows	: 2,
-	columns	: 6
-},
-
-w320			: {
-	rows	: 2,
-	columns	: 6
-},
-
-w240			: {
-	rows	: 2,
-	columns	: 6
-},
-
-// step: number of items that are replaced at the same time
-// random || [some number]
-// note: for performance issues, the number should not be > options.maxStep
-step			: 'random',
-maxStep			: 3,
-
-// prevent user to click the items
-preventClick	: true,
-
-// animation type
-// showHide || fadeInOut || slideLeft || 
-// slideRight || slideTop || slideBottom || 
-// rotateLeft || rotateRight || rotateTop || 
-// rotateBottom || scale || rotate3d || 
-// rotateLeftScale || rotateRightScale || 
-// rotateTopScale || rotateBottomScale || random
-animType		: 'random',
-
-// animation speed
-animSpeed		: 500,
-
-// animation easings
-animEasingOut	: 'linear',
-animEasingIn	: 'linear',
-
-// the item(s) will be replaced every 3 seconds
-// note: for performance issues, the time "can't" be < 300 ms
-interval		: 3000,
-// if false the animations will not start
-// use false if onhover is true for example
-slideshow		: true,
-// if true the items will switch when hovered
-onhover		: false,
-
-				} );
-			
-			});
-		</script>
+                    $('#ri-grid ul>li>a').click(function(evt){
+                        evt.preventDefault();
+                    });
+                };
+            </script>
